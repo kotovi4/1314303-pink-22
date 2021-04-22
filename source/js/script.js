e56115f86113f0889e9b66304aade158870174aa
Mariia <kotovi4.m@gmail.com>
2021-04-22 20:35:21 +0300

    Вносит правки в декоративные элементы главной страницы

--- source/js/script.js
+++ source/js/script.js
@@ -5,15 +5,20 @@ const pageHeader = document.querySelector('.page-header');
 pageHeader.classList.remove('page-header--opened');
 mainNav.classList.remove('main-nav--opened');
 mainNav.classList.add('main-nav--closed');
+mainNavToggle.classList.toggle('main-nav__toggle--close');
 
 mainNavToggle.addEventListener('click', function() {
   if (mainNav.classList.contains('main-nav--closed')) {
     mainNav.classList.remove('main-nav--closed');
     mainNav.classList.add('main-nav--opened');
     pageHeader.classList.add('page-header--opened');
+    pageHeader.classList.remove('page-header--closed');
+    mainNavToggle.classList.toggle('main-nav__toggle--close');
   } else {
     pageHeader.classList.remove('page-header--opened');
+    pageHeader.classList.add('page-header--closed');
     mainNav.classList.remove('main-nav--opened');
     mainNav.classList.add('main-nav--closed');
+    mainNavToggle.classList.toggle('main-nav__toggle--close');
   }
 });
