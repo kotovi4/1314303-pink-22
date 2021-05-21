97dd843ee6d9c549ac988b307a0b961ddbc54a51
Mariia <kotovi4.m@gmail.com>
2021-05-21 15:12:59 +0300

    Вносит правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,22 +1,26 @@
-const mainNav = document.querySelector('.main-nav__list');
+
+const mainNavList = document.querySelector('.main-nav__list');
 const mainNavToggle = document.querySelector('.main-nav__toggle');
 const pageHeader = document.querySelector('.page-header');
 
 pageHeader.classList.remove('page-header--opened');
-mainNav.classList.remove('main-nav--opened');
-mainNav.classList.add('main-nav--closed');
-mainNavToggle.classList.toggle('main-nav__toggle--close');
+mainNavList.classList.remove('main-nav--opened');
+mainNavList.classList.add('main-nav--closed');
+mainNavToggle.classList.remove('main-nav__toggle--closed');
+
 
 mainNavToggle.addEventListener('click', function() {
-  if (mainNav.classList.contains('main-nav--closed')) {
-    mainNav.classList.remove('main-nav--closed');
-    mainNav.classList.add('main-nav--opened');
+  if (mainNavList.classList.contains('main-nav--closed')) {
+    mainNavList.classList.remove('main-nav--closed');
+    mainNavList.classList.add('main-nav--opened');
     pageHeader.classList.add('page-header--opened');
-    mainNavToggle.classList.toggle('main-nav__toggle--close');
+    pageHeader.classList.remove('page-header--closed');
+    mainNavToggle.classList.toggle('main-nav__toggle--closed');
   } else {
     pageHeader.classList.remove('page-header--opened');
-    mainNav.classList.remove('main-nav--opened');
-    mainNav.classList.add('main-nav--closed');
-    mainNavToggle.classList.toggle('main-nav__toggle--close');
+    pageHeader.classList.add('page-header--closed');
+    mainNavList.classList.remove('main-nav--opened');
+    mainNavList.classList.add('main-nav--closed');
+    mainNavToggle.classList.toggle('main-nav__toggle--closed');
   }
 });
