d6dd5699b6e40ab1f1267968a458493613583225
Mariia <kotovi4.m@gmail.com>
2021-05-21 15:28:37 +0300

    Вносит правки в мобильное меню

--- source/js/script.js
+++ source/js/script.js
@@ -6,8 +6,7 @@ const pageHeader = document.querySelector('.page-header');
 pageHeader.classList.remove('page-header--opened');
 mainNavList.classList.remove('main-nav--opened');
 mainNavList.classList.add('main-nav--closed');
-mainNavToggle.classList.remove('main-nav__toggle--closed');
-
+mainNavToggle.classList.remove('main-nav__toggle--hide');
 
 mainNavToggle.addEventListener('click', function() {
   if (mainNavList.classList.contains('main-nav--closed')) {
@@ -15,12 +14,12 @@ mainNavToggle.addEventListener('click', function() {
     mainNavList.classList.add('main-nav--opened');
     pageHeader.classList.add('page-header--opened');
     pageHeader.classList.remove('page-header--closed');
-    mainNavToggle.classList.toggle('main-nav__toggle--closed');
+    mainNavToggle.classList.add('main-nav__toggle--closed');
   } else {
     pageHeader.classList.remove('page-header--opened');
     pageHeader.classList.add('page-header--closed');
     mainNavList.classList.remove('main-nav--opened');
     mainNavList.classList.add('main-nav--closed');
-    mainNavToggle.classList.toggle('main-nav__toggle--closed');
+    mainNavToggle.classList.remove('main-nav__toggle--closed');
   }
 });
