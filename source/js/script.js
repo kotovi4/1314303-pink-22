5cf2d37dc836fa554201c95f6a1da4eace45d21c
kotovi4 <kotovi4.m@gmail.com>
2021-07-22 20:03:30 +0300

    Добавлет маркер

--- source/js/script.js
+++ source/js/script.js
@@ -15,3 +15,54 @@ mainNavToggle.onclick = function() {
   mainNavList.classList.toggle('main-nav--closed');
   mainNavToggle.classList.toggle('main-nav__toggle--closed');
 };
+
+const map = L.map('map')
+  .on('load', () => {
+    console.log('Карта инициализирована');
+  })
+  .setView({
+    lat: 59.936550,
+    lng: 30.32080,
+  }, 15);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/map-marker.svg',
+  iconSize: [36, 36],
+  iconAnchor: [18, 18],
+});
+
+const marker = L.marker(
+  {
+    lat: 59.938635,
+    lng: 30.323118,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+
+marker.on('moveend', (evt) => {
+  console.log(evt.target.getLatLng());
+});
+
+marker.addTo(map);
+
+resetButton.addEventListener('click', () => {
+  mainPinMarker.setLatLng({
+    lat: 59.938635,
+    lng: 30.323118,
+  });
+
+  map.setView({
+    lat: 59.938635,
+    lng: 30.323118,
+  }, 15);
+});
